12974d14c1595b01a7ad64abb4a8a00c119c1850
SalvaBernades <salvador.bernades@estudiantat.upc.edu>
2024-12-07 18:13:32 +0100

    Fixed camera

--- MyGameMaker/MyGameEngine/Camera.cpp
+++ MyGameMaker/MyGameEngine/Camera.cpp
@@ -2,30 +2,29 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 glm::dmat4 Camera::projection() const {
-	return glm::perspective(fov, aspect, zNear, zFar);
+    return glm::perspective(fov, aspect, zNear, zFar);
 }
 
 glm::dmat4 Camera::view() const {
-	return glm::lookAt(_transform.pos(), _transform.pos() + _transform.fwd(), _transform.up());
+    return glm::lookAt(_transform.pos(), _transform.pos() + _transform.fwd(), _transform.up());
 }
 
 std::list<Plane> Camera::frustumPlanes() const {
 
-	const auto h_fov = fov;
-	const auto v_fov = fov / aspect;
-	return {
-		//near
-		Plane(_transform.fwd(), _transform.pos() + _transform.fwd() * zNear),
-		//far
-		Plane(-_transform.fwd(), _transform.pos() + _transform.fwd() * zFar),
-		//left
-		Plane(glm::rotate(-_transform.left(), h_fov, _transform.up()), _transform.pos()),
-		//right
-		Plane(glm::rotate(_transform.left(), -h_fov, _transform.up()), _transform.pos()),
-		//top
-		Plane(glm::rotate(-_transform.up(), -v_fov, _transform.left()), _transform.pos()),
-		//bottom
-		Plane(glm::rotate(_transform.up(), v_fov, _transform.left()), _transform.pos())
-	};
-
+    const auto h_fov = fov;
+    const auto v_fov = fov / aspect;
+    return {
+        // near
+        Plane(_transform.fwd(), _transform.pos() + _transform.fwd() * zNear),
+        // far
+        Plane(-_transform.fwd(), _transform.pos() + _transform.fwd() * zFar),
+        // left
+        Plane(glm::normalize(glm::dvec3(glm::rotate(glm::dmat4(1.0), h_fov, _transform.up()) * glm::dvec4(-_transform.left(), 0.0))), _transform.pos()),
+        // right
+        Plane(glm::normalize(glm::dvec3(glm::rotate(glm::dmat4(1.0), -h_fov, _transform.up()) * glm::dvec4(_transform.left(), 0.0))), _transform.pos()),
+        // top
+        Plane(glm::normalize(glm::dvec3(glm::rotate(glm::dmat4(1.0), -v_fov, _transform.left()) * glm::dvec4(-_transform.up(), 0.0))), _transform.pos()),
+        // bottom
+        Plane(glm::normalize(glm::dvec3(glm::rotate(glm::dmat4(1.0), v_fov, _transform.left()) * glm::dvec4(_transform.up(), 0.0))), _transform.pos())
+    };
 }
